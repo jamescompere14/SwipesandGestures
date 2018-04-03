298c37c41bf70db70f8da7295b894c49638ebf70
jamescompere14 <jamescompere14@augustana.edu>
2018-04-03 17:49:21 -0500

    Made it work

--- app/src/main/java/com/swipesandgestures/csc490/swipesandgestures/MainActivity.java
+++ app/src/main/java/com/swipesandgestures/csc490/swipesandgestures/MainActivity.java
@@ -2,12 +2,91 @@ package com.swipesandgestures.csc490.swipesandgestures;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.GestureDetector;
+import android.support.v4.view.GestureDetectorCompat;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements  GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
+
+    private TextView message;
+    private GestureDetectorCompat gestureDetectorCompat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        message = findViewById(R.id.message);
+        this.gestureDetectorCompat = new GestureDetectorCompat(this,this);
+        gestureDetectorCompat.setOnDoubleTapListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        message.setText("onSingleTapConfirmed");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        message.setText("onDoubleTap");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        message.setText("onDoubleTapEvent");
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        message.setText("onDown");
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        message.setText("onShowPress");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        message.setText("onSingleTapUp");
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        message.setText("onScroll");
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        message.setText("onLongPress");
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        message.setText("onFling");
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        this.gestureDetectorCompat.onTouchEvent(e);
+        return super.onTouchEvent(e);
     }
 }
